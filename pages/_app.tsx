92e6530f4bea15e4fda21a0e4097444c15994c18
moisestech <m@moises.tech>
2023-03-07 19:31:22 -0500

    implement wagmi with rainbowkit Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,59 +1,93 @@
-import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi';
+import { useState, useEffect, useContext } from 'react';
+import AppContext from '../context/AppContext/AppContext';
 
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+import {
+  WagmiConfig,
+  createClient,
+  configureChains,
+  mainnet,
+  useAccount,
+} from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
-import { InjectedConnector } from 'wagmi/connectors/injected';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import {
+  RainbowKitProvider,
+  AvatarComponent,
+  getDefaultWallets,
+} from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
 
-// import { mainnet } from 'wagmi/chains';
+import Blockies from 'react-blockies';
+
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-export const { chains, provider, webSocketProvider } = configureChains(
+console.log(process.env.NEXT_PUBLIC_WALLET_CONNECT_ID);
+
+const { chains, provider } = configureChains(
   [mainnet],
   [
-    alchemyProvider({ apiKey: (process.env.ALCHEMY_ID as string) || '' }),
+    alchemyProvider({
+      apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string,
+    }),
     publicProvider(),
   ]
 );
-
-const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: '...',
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
-  provider,
-  webSocketProvider,
+const { connectors } = getDefaultWallets({
+  appName: 'Eden Art AI',
+  chains,
 });
 
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID as string,
+});
+
+const CustomAvatar: AvatarComponent = ({ address }) => {
+  return <Blockies seed={address} />;
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [isWeb3WalletConnected, setIsWeb3WalletConnected] = useState(false);
+  const [isWeb3AuthSuccess, setIsWeb3AuthSuccess] = useState(false);
+  const [authToken, setAuthToken] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const context = useContext(AppContext);
+
+  Router.events.on('routeChangeStart', nProgress.start);
+  Router.events.on('routeChangeError', nProgress.done);
+  Router.events.on('routeChangeComplete', nProgress.done);
+
+  const contextValues = {
+    authToken,
+    setAuthToken,
+    isWeb3WalletConnected,
+    setIsWeb3WalletConnected,
+    isWeb3AuthSuccess,
+    setIsWeb3AuthSuccess,
+  };
+
+  const { isConnected } = useAccount();
+
+  useEffect(() => {
+    setIsWeb3WalletConnected(isConnected);
+  }, [isConnected, setIsWeb3WalletConnected]);
+
   return (
-    <WagmiConfig client={client}>
-      <Component {...pageProps} />
-    </WagmiConfig>
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider avatar={CustomAvatar} chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 }
