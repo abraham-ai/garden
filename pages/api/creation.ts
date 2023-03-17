0a01f392cfda9186ec6125470f7800058b20bb12
moisestech <m@moises.tech>
2023-03-17 17:52:40 -0400

    creation test page

---
+++ pages/api/creation.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
+import { withSessionRoute } from '../../util/withSession';
+import { eden } from '../../util/eden';
+
+interface ApiRequest extends NextApiRequest {
+  body: {
+    username: string;
+    generators: string[];
+    earliestTime: number;
+    latestTime: number;
+    limit: number;
+  };
+}
+
+const handler = async (req: ApiRequest, res: NextApiResponse) => {
+  // const { username, generators, earliestTime, latestTime, limit } = req.body;
+  const { creationId } = req.query;
+
+  // console.log({ req });
+  console.log(req.url);
+  // console.log(req.body);
+
+  try {
+    Object.assign(filter, username !== 'null' ? { username: username } : {});
+    Object.assign(
+      filter,
+      generators !== 'null' ? { generators: generators } : {}
+    );
+    Object.assign(
+      filter,
+      earliestTime !== 'null' ? { earliestTime: earliestTime } : {}
+    );
+    Object.assign(
+      filter,
+      latestTime !== 'null' ? { latestTime: latestTime } : {}
+    );
+    Object.assign(filter, limit ? { limit: limit } : {});
+
+    console.log({ filter });
+    const creations = await eden.getCreations(filter);
+
+    console.log(creations.length);
+    // console.log(creations);
+
+    return res.status(200).json(creations);
+  } catch (error: any) {
+    console.log(error);
+    // if (error.response.data == 'jwt expired') {
+    //   return res.status(401).json({ error: 'Authentication expired' });
+    // }
+    return res.status(500).json({ error: error });
+    // error.response.data
+  }
+};
+
+export default withSessionRoute(handler);
