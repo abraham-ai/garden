05f68d19280c99af70d481b72388d2170087f5a4
moisestech <m@moises.tech>
2023-03-17 17:54:02 -0400

    init shaURL

---
+++ util/abbreviateAddress.ts
@@ -0,0 +1,6 @@
+const abbreviateAddress = (user: string) => {
+  let displayAddress = user?.substring(0, 6);
+  return (displayAddress += '...' + user.slice(-4));
+};
+
+export default abbreviateAddress;
---
+++ util/abbreviateText.ts
@@ -0,0 +1,8 @@
+const abbreviateText = (text: string, textLength: number) => {
+  if (text.length > textLength) {
+    return text.slice(0, textLength - 3) + '...';
+  }
+  return text;
+};
+
+export default abbreviateText;
