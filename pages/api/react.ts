ff8a2788cfd7919ae93aab3834feb5cdd20bc4e3
moisestech <m@moises.tech>
2023-03-17 17:53:03 -0400

    init api react

---
+++ pages/api/react.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next/types';
+import { withSessionRoute } from '../../util/withSession';
+import { eden } from '../../util/eden';
+
+interface ApiRequest extends NextApiRequest {
+  body: {
+    creationId: string;
+    reaction: string;
+  };
+}
+
+const handler = async (req: ApiRequest, res: NextApiResponse) => {
+  const { creationId, reaction } = req.body;
+  const { token, userId } = req.session;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Not authenticated' });
+  }
+
+  try {
+    const creation = await eden.getCreation(creationId);
+    const result = await creation.react(reaction);
+    return res.status(200).json({ result: result });
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({ error: error.response.data });
+  }
+};
+
+export default withSessionRoute(handler);
