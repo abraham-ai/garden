eb4697c90a328f6c82892223c35e0c2231bfd810
moisestech <m@moises.tech>
2023-03-16 21:04:01 -0400

    creation social types

---
+++ interfaces/CreationSocial.ts
@@ -0,0 +1,10 @@
+interface CreationSocial {
+  layout: string;
+  creationBurns: number;
+  creationPraises: number;
+  creationSha: string;
+  praisedByMe: boolean;
+  burnedByMe: boolean;
+}
+
+export default CreationSocial;
