b35ea66512d46e6eab1208167e066a9b53d16f00
moisestech <m@moises.tech>
2023-04-03 17:50:42 -0400

    updated configs for eslint

--- constants/breakpointColumns.ts
+++ constants/breakpointColumns.ts
@@ -1,6 +1,8 @@
 const breakpointColumnsObj = {
   default: 3,
-  1100: 5,
+  3000: 5,
+  2000: 4,
+  1500: 3,
   700: 2,
   580: 1,
 };
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: false,
+    appDir: false
   },
   reactStrictMode: true,
   swcMinify: true,
@@ -10,16 +10,15 @@ const nextConfig = {
     '@web3modal/ethereum',
     '@web3modal/react',
     '@web3modal/ui',
-    '@web3modal/core',
+    '@web3modal/core'
   ],
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'minio.aws.abraham.fun',
-      },
-    ],
-    // path:'/'
+        hostname: 'minio.aws.abraham.fun'
+      }
+    ]
   },
   env: {
     NEXT_PUBLIC_ALCHEMY_API_KEY: 'hNqWR45RXI-Ek16H1acYUqVbK1GJcV_6',
@@ -27,7 +26,7 @@ const nextConfig = {
     NEXTAUTH_SECRET: 'edenartai',
     NEXT_PUBLIC_WALLET_CONNECT_ID: '044562379c6be56865c30c7ec19034b6',
     COOKIE_SECRET: '1URBSqpQvTa9hR0ClPwdt9DPSuSeMe55'
-  },
-};
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
