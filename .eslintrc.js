67fb09fdfb45fb70f72b414416ce9d7b4a13da65
moisestech <m@moises.tech>
2023-04-04 12:34:31 -0400

    fixing linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
 	env: {
-	  browser: true,
-	  es2021: true,
+		browser: true,
+		es2021: true,
 	},
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
-	  project: "./tsconfig.json",
-	  ecmaVersion: "latest",
-	  sourceType: "module",
+		project: "./tsconfig.json",
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
 	plugins: ["react", "@typescript-eslint"],
 	extends: [
-	  "plugin:react/recommended",
-	  "standard-with-typescript",
-	  "eslint:recommended",
-	  "plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"standard-with-typescript",
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		// "plugin:prettier/recommended"
 	],
 	rules: {
-	  "@typescript-eslint/dot-notation": "error",
-	  "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+		"@typescript-eslint/dot-notation": "error",
+		"react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+		"indent": ["error", "tab"],
+		"@typescript-eslint/indent": "off",
+		// "prettier/prettier": ["error", {}, { usePrettierrc: true }]
 	},
 };
