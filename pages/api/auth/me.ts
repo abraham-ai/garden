1ddb585a9414b5e93189b718ef336ff6d9197267
moisestech <m@moises.tech>
2023-04-17 17:54:42 -0400

    setting userId

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -7,27 +7,15 @@ const handler = async (
 	res: NextApiResponse
 ): Promise<void> => {
 	// Get the user from the session
-	const user = req.session.user
+	const userId = req.session.userId
 
-	if (!user) {
+	if (!userId) {
 		res.status(401).json({ message: 'Not authenticated' })
 		return
 	}
 
 	// Return the user data
-	res.status(200).json({ user })
-
-	// switch (method) {
-	// 	case 'GET':
-	// 		const token = session.get('token')
-	// 		const userId = session.get('userId')
-
-	// 		res.send({ userId, token })
-	// 		break
-	// 	default:
-	// 		res.setHeader('Allow', ['GET'])
-	// 		res.status(405).end(`Method ${String(method)} Not Allowed`)
-	// }
+	res.status(200).json({ userId })
 }
 
 export default withSessionRoute(handler)
