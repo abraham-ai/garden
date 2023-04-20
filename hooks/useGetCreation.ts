e3821300fffe8ef8ad6dd59292191f7c6f102967
moisestech <m@moises.tech>
2023-04-20 14:38:02 -0400

    read data properly from response

--- hooks/useGetCreation.ts
+++ hooks/useGetCreation.ts
@@ -6,20 +6,26 @@ import axios from 'axios'
 const useGetCreation = (creationId: string): Creation | null => {
 	const [creation, setCreation] = useState<Creation | null>(null)
 
+	const isCreationId =
+		typeof creationId !== 'undefined' &&
+		creationId !== null &&
+		creationId !== ''
+
 	const handleGetCreation = useCallback(async (creationId) => {
 		console.log(`useGetCreation: creationId: ${String(creationId)}`)
 		const response = await axios.post('/api/creation', {
 			creationId,
 		})
 
+		const { creation } = response.data
 		// console.log(response.data);
 		console.log('useGetCreation: response.data:')
-		console.log(response)
-		setCreation(response.data)
+		console.log(creation)
+		setCreation(creation)
 	}, [])
 
 	useEffect(() => {
-		if (typeof creationId !== 'undefined' && creationId !== null) {
+		if (isCreationId) {
 			handleGetCreation(creationId).catch((error) => {
 				console.error('Error fetching creation:', error)
 			})
